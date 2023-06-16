d068582db01a7af826708270f31684366e97ea62
Tristan Pernot <pernottristan@gmail.com>
2023-06-16 10:29:52 -0700

    Started on community page

---
+++ src/pages/CommunityPage/CommunityPage.jsx
@@ -0,0 +1,15 @@
+import './CommunityPage.scss'
+function CommunityPage() {
+    return(
+        <div className="container">
+            <div className="container__header">
+                <ul>
+                    
+                </ul>
+            </div>
+        </div>
+
+    );
+}
+
+export default CommunityPage
